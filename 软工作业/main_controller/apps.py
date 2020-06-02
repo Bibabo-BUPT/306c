0c591c45929e00a9bc9cca01fd9dedcf373afa08
Bibabo-BUPT <zhangjialu_cs@bupt.edu.cn>
2020-06-02 10:05:21 +0800

    admin and frame

--- 软工作业/main_controller/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MainControllerConfig(AppConfig):
-    name = 'main_controller'
--- 软工作业/main_controller/bill.py
+++
@@ -1,8 +0,0 @@
-class bill:
-    def __init__(self,customer,room_no,total_fee):
-        self.customer=customer
-        self.room_no=room_no
-        self.total_fee=total_fee
-
-    def print_bill(self,room_no):
-        return 0
